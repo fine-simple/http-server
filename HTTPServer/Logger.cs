a03f3fde63196e20c06a84c337cea18e0f79dc23
Ahmed Tawfik <at200000000@gmail.com>
2021-12-29 11:08:09 +0200

    Implemented Semaphore for Logger

--- HTTPServer/Logger.cs
+++ HTTPServer/Logger.cs
@@ -3,21 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Threading;
 
 namespace HTTPServer
 {
     class Logger
     {
-        static object lockObject = new object();
+        static Semaphore fileSemaphore = new Semaphore(1,1);
         public static void LogException(Exception ex)
         {
             //Datetime:
             DateTime dateTime = DateTime.Now;
             // log exception details associated with datetime
-            lock(lockObject)
-            {
-                File.AppendAllText("log.txt", $"{dateTime}: {ex.Message} at {ex.TargetSite}\n");
-            }
+            fileSemaphore.WaitOne();
+            File.AppendAllText("log.txt", $"{dateTime}: {ex.Message} at {ex.TargetSite}\n");
+            fileSemaphore.Release();
         }
     }
 }
